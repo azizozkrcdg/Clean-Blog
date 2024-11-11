0dc38a59ef8812ba9cee454af1edaf2ab6557dbe
aziz özkaracadağ <azizozkaracadaag@gmail.com>
2024-11-11 19:51:01 +0300

    workspace created

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+const blog = { id: 1, title: "Blog title", description: "Blog description" }
+
+app.get("/", (req, res) => {
+  res.send(blog);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`The server is running on port ${port}`);
+});
